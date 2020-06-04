2f7e1c1e9312aba4587ae18ceaea3248fbe71acb
tyq <2418511147@qq.com>
2020-06-04 15:47:46 +0800

    商城

---
+++ src/views/home/Home.vue
@@ -0,0 +1,165 @@
+<template>
+ <div id="home">
+   <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+   <home-swiper :banners="banners"></home-swiper>
+   <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :titles="['流行', '新款', '精选']"></tab-control>
+     <ul>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+       <li></li>
+     </ul>
+ </div>
+ 
+</template>
+
+<script>
+
+import HomeSwiper from './childComps/HomeSwiper'
+import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView'
+
+import TabControl from 'components/content/tabControl/TabControl'
+import NavBar from 'components/common/navbar/NavBar'
+
+import {getHomeMultidata} from 'network/home';
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+      TabControl
+    },
+    data () {
+      return { //用来保存请求过来的数据
+          banners: [],
+          recommends: []
+
+      }
+
+    },
+    created() {
+      //请求数据
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
+    }
+  }
+</script>
+
+<style scoped>
+#home {
+  padding-top:44px;
+}
+.home-nav {
+  background-color: var(--color-tint) ;
+  color:white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index:9999;
+}
+</style>
